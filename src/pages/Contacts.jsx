917f388fa6d32cb8bb952e72e657b53c621ca665
cbogart91 <166065354+cbogart91@users.noreply.github.com>
2024-09-18 12:15:51 -0500

    recommitting, forgot to edit contacts page

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,10 +1,6 @@
 export default function Contacts(){
     return(
         <div>
-       <div className="App">
-        <h1>Contact me</h1>
-        <ContactForm />
-       </div>
     </div>
     );
 }
