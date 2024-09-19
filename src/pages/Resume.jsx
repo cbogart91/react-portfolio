6650b108ec1fd1dbab816643478753b185990110
cbogart91 <166065354+cbogart91@users.noreply.github.com>
2024-09-19 10:00:35 -0500

    download resume works

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,11 @@
+import resume from '/utils/resume2.pdf'
+
 export default function Resume(){
   return(
-    <p></p>
+    <div className='h-[100vh] flex justify center items-center'>
+      <button className='bg-slate-400 px-6 py-2 rounded'>
+      <a href={resume} download='resume'>Download</a>
+      </button>
+      </div>
   );
 }
