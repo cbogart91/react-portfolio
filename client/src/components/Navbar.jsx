645e31d1d5aa456ab18741c94fa319e6a66615da
cbogart91 <166065354+cbogart91@users.noreply.github.com>
2024-11-04 14:14:49 -0600

    making changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,6 +7,8 @@ function Navbar({ navOpen }){
 
 
     const initActiveBox = () => {
+        console.log(lastActiveLink.current)
+        console.log(activeBox.current)
         activeBox.current.style.top = lastActiveLink.current.offsetTop + 'px';
         activeBox.current.style.left = lastActiveLink.current.offsetLeft + 'px';
         activeBox.current.style.width = lastActiveLink.current.offsetWidth + 'px';
@@ -18,6 +20,11 @@ function Navbar({ navOpen }){
         lastActiveLink.current?.classList.remove('active');
         event.target.classList.add('active');
         lastActiveLink.current = event.target;
+
+        activeBox.current.style.top = event.target.offsetTop + 'px';
+        activeBox.current.style.left = event.target.offsetLeft + 'px';
+        activeBox.current.style.width = event.target.offsetWidth + 'px';
+        activeBox.current.style.height = event.target.offsetHeight + 'px';
     }
 
     const navItems = [
